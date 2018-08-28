5b3b54edcd34caf2e18b8cbf78a9664447a9e521
deco <deco@cubee.com>
2018-08-28 14:09:05 +0800

    work7/__init__.py

--- work7/__init__.py
+++ work7/__init__.py
@@ -4,9 +4,14 @@
 @Author    : Deco [deco@cubee.com]
 @Created   : 8/28/18 1:19 PM
 @Desc      : natural language understanding
+
+事件抽取（实体抽取），文本分类
 prerequisite: sentence vector, paragraph vector
-nonparametric: k nearest neighbors, decision tree, emd
+nonparametric: k nearest neighbors, decision tree, emd（地球物理）
 parametric: linear regression, logistic regression, softmax regression,
             support vector machine, neural net, cnn, rnn
+
 deep learning: machine reading comprehension, gan, lang2sql
+
+multi-round conversation: rasa-core
 """
