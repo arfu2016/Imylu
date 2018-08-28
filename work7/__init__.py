8f347206388b547dd83849e632641c17ecc0821e
deco <deco@cubee.com>
2018-08-28 13:35:31 +0800

    work7/__init__.py

---
+++ work7/__init__.py
@@ -0,0 +1,12 @@
+"""
+@Project   : Imylu
+@Module    : __init__.py.py
+@Author    : Deco [deco@cubee.com]
+@Created   : 8/28/18 1:19 PM
+@Desc      : natural language understanding
+prerequisite: sentence vector, paragraph vector
+nonparametric: k nearest neighbors, decision tree, emd
+parametric: linear regression, logistic regression, softmax regression,
+            support vector machine, neural net, cnn, rnn
+deep learning: machine reading comprehension, gan, lang2sql
+"""
